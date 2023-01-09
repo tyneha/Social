dcbf0600f64a3c7617e8d9f6f890ec38c15566ca
tyneha <tyneha56@gmail.com>
2023-01-09 19:20:47 +0530

    first commit

---
+++ client/src/pages/Profile/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ProfileLeft from '../../components/ProfileLeft/ProfileLeft'
+import PostSide from '../../components/PostSide/PostSide.jsx'
+import ProfileCard from '../../components/ProfileCard/ProfileCard'
+import RightSide from '../../components/RightSide/RightSide'
+import './Profile.css'
+
+const Profile = () => {
+  return (
+    <div className='Profile'>
+       <ProfileLeft/>
+
+       <div className='Profile-center'>
+          <ProfileCard location="profilePage"/>
+          <PostSide/>
+       </div>
+    <RightSide/>
+    </div>
+  )
+}
+
+export default Profile
